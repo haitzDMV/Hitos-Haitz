7d76c21c9a66c654fc284d17e8c957b9ebd2646f
ik012982i9 <a@>
2024-03-14 13:12:06 +0100

    Radiobuttons añadidos

--- src/GUI.java
+++ src/GUI.java
@@ -31,7 +31,62 @@ public class GUI {
         }
         container.add(centerPanel, BorderLayout.CENTER);
 
+        //--------Panel de los botones--------
+        JPanel eastPanel = new JPanel(null);
+        eastPanel.setPreferredSize(new Dimension(250, 0));
 
+        //Creo grupo de botones
+        ButtonGroup botones = new ButtonGroup();
+
+        //Creo array de botones
+        JRadioButton[] boton=new JRadioButton[3];
+
+        //Creo los botones que habrá que añadir al array
+        JRadioButton boton1 = new JRadioButton("OPT1");
+        JRadioButton boton2 = new JRadioButton("OPT2");
+        JRadioButton boton3 = new JRadioButton("OPT3");
+
+        //Añado los botones al array
+        boton[0]=boton1;
+        boton[1]=boton2;
+        boton[2]=boton3;
+
+        //Añado los botones del array al grupo de botones
+        botones.add(boton[0]);
+        botones.add(boton[1]);
+        botones.add(boton[2]);
+
+        //le doy posicion a los botones
+        boton[0].setBounds(10, 100, 100, 30);
+        boton[1].setBounds(10, 130, 100, 30);
+        boton[2].setBounds(10, 160, 100, 30);
+
+        //Añado los botones al panel
+        eastPanel.add(boton[0]);
+        eastPanel.add(boton[1]);
+        eastPanel.add(boton[2]);
+
+        //Pongo el primer boton como seleccionado
+        botones.setSelected(boton[0].getModel(),true);
+
+        container.add(eastPanel, BorderLayout.EAST);
+
+        /*
+        //Añadir RadioButtons al grupo
+        botones.add(boton1);
+        botones.add(boton2);
+        botones.add(boton3);
+        boton1.setBounds(10, 100, 100, 30);
+        boton2.setBounds(10, 130, 100, 30);
+        boton3.setBounds(10, 160, 100, 30);
+
+        //Añadir RadioButtons al panel
+        eastPanel.add(boton1);
+        eastPanel.add(boton2);
+        eastPanel.add(boton3);
+        container.add(eastPanel, BorderLayout.EAST);
+
+         */
 
 
 
