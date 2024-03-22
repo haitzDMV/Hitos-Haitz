a92bd850eeaec3a0a4edb8e2d6dd80e919210a56
Haitz <a@a.a>
2024-03-22 09:54:26 +0100

    Hito 2 mas o menos terminado

--- src/Hito2/hito2.java
+++ src/Hito2/hito2.java
@@ -23,10 +23,13 @@ public class hito2 {
         String[] txt = {"python.txt", "c.txt", "java.txt", "inventado.txt"};
         JComboBox<String> fich = new JComboBox<>(txt);
         fich.setSelectedItem("java.txt");
+        JButton clearButton = new JButton("Clear");
+
 
         //---------------Zona derecha---------------
 
         JPanel drch = new JPanel();
+        drch.setLayout(new BorderLayout());
         JTextArea muchoTexto = new JTextArea();
         //muchoTexto.setPreferredSize(new Dimension(400, 400));
         muchoTexto.setLineWrap(true);
@@ -34,9 +37,13 @@ public class hito2 {
 
 
         JScrollPane sp = new JScrollPane(muchoTexto);
-        sp.setPreferredSize(new Dimension(450, 450));
+        sp.setPreferredSize(new Dimension(450, 430));
         sp.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
         sp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+        JButton close = new JButton("Close");
+        close.setPreferredSize(new Dimension(40,30));
+
 
         //---------------Acciones---------------
 
@@ -48,15 +55,35 @@ public class hito2 {
                     String contenido = new String(Files.readAllBytes(Paths.get(fichSeleccionado)));
                     muchoTexto.setText(contenido);
                 } catch (IOException ex) {
-                    muchoTexto.setText("No se puede leer el archivo: " + ex.getMessage() + ".");
+                    JOptionPane.showMessageDialog(null,"Error, el fichero no se puede leer.");
+                    //muchoTexto.setText("No se puede leer el archivo: " + ex.getMessage() + ".");
                 }
             }
         });
 
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                muchoTexto.setText("");
+            }
+        });
+
+        close.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ventana.dispose();
+                System.exit(0);
+            }
+        });
+
+        ////---------------Añadir---------------
+
         izq.add(fich);
+        izq.add(clearButton);
         contenedor.add(izq, BorderLayout.WEST);
 
-        drch.add(sp);
+        drch.add(sp,BorderLayout.NORTH);
+        drch.add(close,BorderLayout.SOUTH);
         contenedor.add(drch, BorderLayout.EAST);
 
         ventana.pack();
