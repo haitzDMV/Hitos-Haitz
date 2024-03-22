9c18ae62ad9ced875c9618368e590be1724632e8
Haitz <a@a.a>
2024-03-22 08:38:02 +0100

    Hito 2 añadir zona izquierda y derecha (JComboBox y JText area)

--- src/Hito2/hito2.java
+++ src/Hito2/hito2.java
@@ -9,11 +9,33 @@ public class hito2 {
         ventana.setPreferredSize(new Dimension(900,500));
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        Container contenedor=ventana.getContentPane();
+        contenedor.setLayout(new BorderLayout());
+
+        //---------------Zona izquierda---------------
+
+        JPanel izq = new JPanel();
+        String[] txt = {"python.txt","c.txt","java.txt"};
+        JComboBox fich = new JComboBox<>(txt);
 
 
 
+        izq.add(fich);
+        contenedor.add(izq,BorderLayout.WEST);
 
 
+
+        //---------------Zona derecha---------------
+
+        JPanel drch = new JPanel();
+        JTextField muchoTexto=new JTextField("AAA");
+        muchoTexto.setPreferredSize(new Dimension(450,450));
+        muchoTexto.setEditable(false);
+
+
+
+        drch.add(muchoTexto);
+        contenedor.add(drch,BorderLayout.EAST);
         //ventana.setLocationRelativeTo(null);
         ventana.pack();
         ventana.setVisible(true);
