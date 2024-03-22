472495587c2f8d62fdb5cd338e2978ab0b364ed0
Haitz <a@a.a>
2024-03-22 09:27:48 +0100

    Hito 2 JScrollBar incident solucionado

--- src/Hito2/hito2.java
+++ src/Hito2/hito2.java
@@ -11,72 +11,58 @@ import java.nio.file.Paths;
 public class hito2 {
     public static void hito2() {
         JFrame ventana = new JFrame("Test Events: Files");
-        ventana.setPreferredSize(new Dimension(900,500));
+        ventana.setPreferredSize(new Dimension(900, 500));
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        Container contenedor=ventana.getContentPane();
+        Container contenedor = ventana.getContentPane();
         contenedor.setLayout(new BorderLayout());
 
         //---------------Zona izquierda---------------
 
         JPanel izq = new JPanel();
-        String[] txt = {"python.txt","c.txt","java.txt","inventado.txt"};
-        JComboBox fich = new JComboBox<>(txt);
+        String[] txt = {"python.txt", "c.txt", "java.txt", "inventado.txt"};
+        JComboBox<String> fich = new JComboBox<>(txt);
         fich.setSelectedItem("java.txt");
 
         //---------------Zona derecha---------------
 
         JPanel drch = new JPanel();
-        JTextArea muchoTexto=new JTextArea();
-        muchoTexto.setPreferredSize(new Dimension(400,400));
+        JTextArea muchoTexto = new JTextArea();
+        //muchoTexto.setPreferredSize(new Dimension(400, 400));
         muchoTexto.setLineWrap(true);
         muchoTexto.setEditable(false);
 
+
         JScrollPane sp = new JScrollPane(muchoTexto);
+        sp.setPreferredSize(new Dimension(450, 450));
         sp.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
         sp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-
-
 
         //---------------Acciones---------------
 
         fich.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String fichSeleccionado = (String)fich.getSelectedItem();
+                String fichSeleccionado = (String) fich.getSelectedItem();
                 try {
                     String contenido = new String(Files.readAllBytes(Paths.get(fichSeleccionado)));
                     muchoTexto.setText(contenido);
-                } catch(IOException ex) {
-                    muchoTexto.setText("No se puede leer el archivo: "+ex.getMessage()+".");
+                } catch (IOException ex) {
+                    muchoTexto.setText("No se puede leer el archivo: " + ex.getMessage() + ".");
                 }
             }
         });
 
-
-
-
-
-
-
-
-
         izq.add(fich);
-        contenedor.add(izq,BorderLayout.WEST);
-
-
-
-
-
-
-
+        contenedor.add(izq, BorderLayout.WEST);
 
         drch.add(sp);
-        contenedor.add(drch,BorderLayout.EAST);
-        //ventana.setLocationRelativeTo(null);
+        contenedor.add(drch, BorderLayout.EAST);
+
         ventana.pack();
         ventana.setVisible(true);
     }
+
     public static void main(String[] args) {
         hito2();
     }
